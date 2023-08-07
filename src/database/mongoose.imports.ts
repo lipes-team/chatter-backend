3733d27bcf80163e834fb2af5d571d89eab89ab3
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-08-07 12:02:46 +0200

    extract mongoose imports to different file
    Co-authored-by: Filipe Ferreira <filipemgf@users.noreply.github.com>

---
+++ src/database/mongoose.imports.ts
@@ -0,0 +1,25 @@
+import {
+	Document,
+	Schema,
+	model,
+	InferSchemaType,
+	HydratedDocumentFromSchema,
+	Types,
+	HydratedDocument,
+	connect,
+	disconnect,
+	Model,
+} from 'mongoose';
+
+export {
+	Document,
+	Schema,
+	model,
+	InferSchemaType,
+	HydratedDocumentFromSchema,
+	Types,
+	HydratedDocument,
+	connect,
+	disconnect,
+	Model,
+};
