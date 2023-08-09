dcf8bd44301498baa39f0487b1de8d6f63e6dfcf
Filipe Ferreira <filipeferreira94@gmail.com>
2023-08-09 12:18:03 +0100

    feat(tests): add findOne, update, deleteOne and deleteMany
    Co-authored-by: Felipe Mantovani <f-mantovani@users.noreply.github.com>

--- tests/database.test.ts
+++ tests/database.test.ts
@@ -1,8 +1,16 @@
 import { Connection } from 'mongoose';
 
-import { addToDb, find, findOne, update } from '../src/database/abstraction';
+import {
+	addToDb,
+	find,
+	findOne,
+	update,
+	deleteOne,
+	deleteMany,
+} from '../src/database/abstraction';
 import { userModel } from '../src/models/User.model';
 import { connectDB, disconnectDB } from '../src/database/connection';
+import { logger } from '../src/utils/logger';
 
 describe('Mongoose Abstraction', () => {
 	let database: Connection | undefined;
@@ -13,9 +21,11 @@ describe('Mongoose Abstraction', () => {
 		password: 'secretpass123',
 		email: 'johndoe@test.com',
 	};
+	const name = 'Alice Bob';
 	beforeAll(async () => {
 		database = await connectDB();
 		expect(database).toBeDefined();
+		await Model.ensureIndexes();
 	});
 
 	afterAll(async () => {
@@ -26,19 +36,84 @@ describe('Mongoose Abstraction', () => {
 	});
 
 	it('create abstraction', async () => {
-		const testUser = await addToDb(Model, userTestData);
+		try {
+			const testUser = await addToDb(Model, userTestData);
 
-		// Check if the test user matches the expected data without timestamps
-		expect(testUser).toMatchObject(userTestData);
+			// Check if the test user matches the expected data without timestamps
+			expect(testUser).toMatchObject(userTestData);
+		} catch (error) {
+			logger.error(error);
+		}
 	});
 
 	it('find abstraction', async () => {
-		const testUser = await find(Model, { name: userTestData.name }).select(
-			'+password'
-		);
-
-		expect(testUser[0]).toMatchObject(userTestData);
+		try {
+			const testUser = await find(Model, { name: userTestData.name }).select(
+				'+password'
+			);
+			expect(testUser[0]).toMatchObject(userTestData);
+		} catch (error) {
+			logger.error(error);
+		}
 	});
 
-	it('', async () => {});
+	it('find one abstraction', async () => {
+		try {
+			const testUser = await findOne(Model, { name: userTestData.name }).select(
+				'+password'
+			);
+			expect(testUser).toMatchObject(userTestData);
+		} catch (error) {
+			logger.error(error);
+		}
+	});
+
+	it('update abstraction', async () => {
+		try {
+			const testUser = await update(
+				Model,
+				{ name: userTestData.name },
+				{ name },
+				{ new: true }
+			).select('+password');
+			const userUpdated = { ...userTestData };
+			userUpdated.name = name;
+			expect(testUser).toMatchObject(userUpdated);
+		} catch (error) {
+			logger.error(error);
+		}
+	});
+
+	it('delete abstraction', async () => {
+		try {
+			const res = await deleteOne(Model, { name });
+
+			expect(res).toMatchObject({ acknowledged: true, deletedCount: 1 });
+		} catch (error) {
+			logger.error(error);
+		}
+	});
+
+	it('delete many abstraction', async () => {
+		const users = [
+			{
+				name: 'John Doe',
+				password: 'secretpass123',
+				email: 'johndoe@test.com',
+			},
+			{
+				name: 'John Doe',
+				password: 'secretpass123',
+				email: 'johndoe2@test.com',
+			},
+		];
+
+		try {
+			await addToDb(Model, users);
+			const res = await deleteMany(Model, { name: userTestData.name });
+			expect(res).toMatchObject({ acknowledged: true, deletedCount: 2 });
+		} catch (error) {
+			logger.error(error);
+		}
+	});
 });
