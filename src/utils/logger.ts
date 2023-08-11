bee00fbbcee70fd5a1a99315721b06c6aee77fb6
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-08-11 19:06:51 +0200

    minor(pretty): reestructure so the openHandles were closing after the tests

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,24 +1,25 @@
-import { NextFunction, Request, Response } from "express";
-import pino from "pino";
+import { NextFunction, Request, Response } from 'express';
+import pino from 'pino';
+import pretty from 'pino-pretty';
 
-const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    }
-  }
-}) 
+const logger = pino(
+	pretty({
+		colorize: true,
+		timestampKey: 'time',
+		translateTime: `SYS:dd.mm.yy HH:MM`,
+		colorizeObjects: true,
+	})
+);
 
 function requestLogger(req: Request, res: Response, next: NextFunction) {
-  res.on("finish", () => {
-    if (res.statusCode >= 400 && res.statusCode <= 600) {
-    logger.error(`${req.method} (status ${res.statusCode}) at ${req.path}`)
-  } else {
-    logger.info(`${req.method} (status ${res.statusCode}) at ${req.path}`)
-  }
-  })
-  next()
+	res.on('finish', () => {
+		if (res.statusCode >= 400 && res.statusCode <= 600) {
+			logger.error(`${req.method} (status ${res.statusCode}) at ${req.path}`);
+		} else {
+			logger.info(`${req.method} (status ${res.statusCode}) at ${req.path}`);
+		}
+	});
+	next();
 }
 
 export { logger, requestLogger };
