acf423c268fcfb9d1e5156925159c936ff2409d1
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-08-14 13:10:42 +0200

    feature(error handling):Sending the correct errors with zod too

--- src/routes/errorHandling.routes.ts
+++ src/routes/errorHandling.routes.ts
@@ -2,6 +2,17 @@ import { logger } from '../utils/logger';
 import { RouteOpts } from '../utils/types';
 
 const errorHandling = (error: RouteOpts['error'], res: RouteOpts['res']) => {
+	if (error.issues) {
+		logger.error(error.issues);
+		const errors = error.issues.map((error) => ({
+			message: error.message,
+			expected: error.expected,
+			received: error.received,
+			path: error.path,
+		}));
+		return res.status(error.status || 500).json({ errors, path: error.path });
+	}
+
 	logger.error(error);
 	return res
 		.status(error.status || 500)
