98ca55f013f78a9ee093809d9b62a68afdb4908a
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-08-16 11:53:16 +0200

    minor(expect abstraction): removed the unecessary casting for the res.body

--- tests/utils/expectAbstractions.ts
+++ tests/utils/expectAbstractions.ts
@@ -1,7 +1,5 @@
 import { Response } from 'supertest';
 
-type ResponseBody<Expected> = Expected extends Array<infer T> ? T : Expected;
-
 export const expectStatus = (res: Response, status: number) => {
 	expect(res.status).toEqual(status);
 };
@@ -10,6 +8,6 @@ export const expectResponseBody = <Expected extends {}>(
 	res: Response,
 	expectedObject: Expected
 ) => {
-	const responseBody = res.body as ResponseBody<Expected>;
+	const responseBody = res.body;
 	expect(responseBody).toMatchObject(expectedObject);
 };
