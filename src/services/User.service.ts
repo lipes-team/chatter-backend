24de51b34a79c29508b04c6f3a3cca4d7f122a71
Filipe Ferreira <filipeferreira94@gmail.com>
2023-08-21 16:42:58 +0100

    feat(userService): password hashing and comparison to plain

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -11,10 +11,17 @@ import {
 	NoTimestamps,
 } from '../database/abstraction';
 
+import bcrypt from 'bcrypt';
+
 type Filter = FilterOptions<User>;
 type Update = UpdateOptions<User>;
 type Options = OptionsQuery<User>;
-type UserData = NoTimestamps<User>;
+/* type UserData = NoTimestamps<User>; Comentei esta e criei uma intertface simples */
+interface UserData {
+	name: string;
+	password: string;
+	email: string;
+}
 
 class UserService {
 	userModel: UserModel;
@@ -23,11 +30,36 @@ class UserService {
 		this.userModel = userModel;
 	}
 	async createUser(newUser: UserData) {
+		let hashedPassword = await this.hashPassword(newUser);
+		newUser.password = hashedPassword;
 		return addToDb(this.userModel, newUser);
 	}
 
-	async checkUnique(newUser: UserData) {
-		return findOne(this.userModel, newUser);
+	async hashPassword(newUser: UserData): Promise<string> {
+		return new Promise((resolve, reject) => {
+			bcrypt.hash(newUser.password, 10, function (error, hash) {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(hash);
+				}
+			});
+		});
+	}
+
+	async compareHashedPassword(
+		plainPassword: string,
+		hashPassword: string
+	): Promise<boolean> {
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(plainPassword, hashPassword, function (error, result) {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(result);
+				}
+			});
+		});
 	}
 }
 
