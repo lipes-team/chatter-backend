561595e05baaaa67658af3a742716d99ba4b3d7f
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-09-18 14:30:13 +0200

    fix(isAutheticated): fixing imports and exports from the middleware

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { postController } from '../controllers/Post.controller';
 import { validateSchema } from '../middlewares/validateSchema';
 import { createPostSchema } from '../validation/post.schema';
+import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const router = Router();
 
+router.use(isAuthenticated);
 router.post('/', validateSchema(createPostSchema), postController.create);
 
 export { router as postRoutes };
