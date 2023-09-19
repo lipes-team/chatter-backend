c82f0a0ce8828324e35b3706e00eca2b1403acec
Filipe Ferreira <filipeferreira94@gmail.com>
2023-09-19 10:53:04 +0100

    feat(login): login input is validated by zod

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { validateSchema } from '../middlewares/validateSchema';
 
-import { createUserSchema } from '../validation/user.schema';
+import { createUserSchema, loginUserSchema } from '../validation/user.schema';
 import { userController } from '../controllers/User.controller';
 
 const router = Router();
 
 router.post('/signup', validateSchema(createUserSchema), userController.create);
 
-router.post('/login', userController.login);
+router.post('/login', validateSchema(loginUserSchema), userController.login);
 
 export { router as userRoutes };
