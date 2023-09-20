40471bf94599182cbd5fc8a3928a574dad54aac1
Filipe Ferreira <filipeferreira94@gmail.com>
2023-09-20 11:59:55 +0100

    minor(update): add auth middleware to route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,11 +3,14 @@ import { validateSchema } from '../middlewares/validateSchema';
 
 import { createUserSchema, loginUserSchema } from '../validation/user.schema';
 import { userController } from '../controllers/User.controller';
+import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 const router = Router();
 
 router.post('/signup', validateSchema(createUserSchema), userController.create);
 
 router.post('/login', validateSchema(loginUserSchema), userController.login);
+
+router.post("/update", isAuthenticated, userController.update)
 
 export { router as userRoutes };
