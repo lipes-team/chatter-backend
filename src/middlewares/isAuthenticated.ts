3ab6bee03892f6faeb919b5a6a1ae8e95398ac55
Filipe Ferreira <filipeferreira94@gmail.com>
2023-09-20 11:58:39 +0100

    minor(auth): change " to '

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,25 +1,22 @@
-import { Request } from "express-jwt";
+import { Request } from 'express-jwt';
 
-var { expressjwt: jwt } = require("express-jwt");
+import { expressjwt as jwt } from 'express-jwt';
 
-const isAuthenticated = jwt({
-    secret: process.env.JWT_SECRET || "d3f4ults3cr3t",
-    algorithms: ["HS256"],
-    requestProperty: "payload",
-    getToken: getTokenFromHeaders
+export const isAuthenticated = jwt({
+    secret: process.env.JWT_SECRET || 'd3f4ults3cr3t',
+    algorithms: ['HS256'],
+    requestProperty: 'payload',
+    getToken: getTokenFromHeaders,
 });
 
 function getTokenFromHeaders(req: Request) {
-
-    if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
-
-        const token = req.headers.authorization.split(" ")[1];
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'Bearer'
+    ) {
+        const token = req.headers.authorization.split(' ')[1];
         return token;
     }
 
-    return null;
-}
-
-module.exports = {
-    isAuthenticated
+    return undefined;
 }
