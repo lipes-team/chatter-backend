fd6f120c701d88e38b7bf1da9bead3f964904a7f
Filipe Ferreira <filipeferreira94@gmail.com>
2023-09-20 10:35:00 +0100

    minor(service): hashPass and compareHashPass simplified

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -37,36 +37,20 @@ class UserService {
 	}
 
 	async hashPassword(newUser: UserData): Promise<string> {
-		return new Promise((resolve, reject) => {
-			bcrypt.hash(newUser.password, 10, function (error, hash) {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(hash);
-				}
-			});
-		});
+
+		return bcrypt.hash(newUser.password, 10)
+
 	}
 
 	async compareHashedPassword(
 		plainPassword: string,
 		hashPassword: string
 	): Promise<boolean> {
-		return new Promise((resolve, reject) => {
-			bcrypt.compare(plainPassword, hashPassword, function (error, result) {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(result);
-				}
-			});
-		});
+		return bcrypt.compare(plainPassword, hashPassword)
 	}
 
-	async findUser(email: string) {
-		return findOne(userModel, { email }).select(
-			'+password'
-		);
+	async findUser(filter: Filter, options?: Options) {
+		return findOne(userModel, filter, options)
 	}
 
 	createAuthToken(user: Remover<UserData, "password">) {
@@ -82,8 +66,8 @@ class UserService {
 		});
 	}
 
-	updateUser(oldEmail: string, newUser: UserData) {
-		return update(userModel, { email: oldEmail }, newUser, { new: true, lean: true })
+	updateUser(filter: Filter, newUser: Update, options?: Options) {
+		return update(userModel, filter, newUser, { new: true, lean: true, ...options })
 	}
 }
 
