80db9f17741174f18c71a63dd3f788140bbc15d3
Filipe Ferreira <filipeferreira94@gmail.com>
2023-09-27 17:52:30 +0100

    feat(test): created test suite & add create controller test

---
+++ tests/group.test.ts
@@ -0,0 +1,77 @@
+import { Express } from 'express';
+import { Connection } from 'mongoose';
+
+import { initializeApp } from '../app';
+import { disconnectDB } from '../src/database/connection';
+import { groupModel } from '../src/models/Group.model';
+import { userService } from '../src/services/User.service';
+import { logger } from '../src/utils/logger';
+import { postRequest } from './utils/requestAbstraction';
+import { expectStatus } from './utils/expectAbstractions';
+
+
+describe.only('User Services', () => {
+    let database: Connection | undefined;
+    let app: Express | undefined;
+    let authToken = "";
+
+    beforeAll(async () => {
+        try {
+            const { app: application, db } = await initializeApp();
+            database = db;
+            app = application;
+            await groupModel.ensureIndexes();   //ensure mongoose validation based on userModel
+
+            let userInfo = {
+                name: 'Jane Doe',
+                password: 'TestTest123',
+                email: 'uniquejane@email.com',
+            };
+
+            await userService.createUser(userInfo);
+
+            authToken = userService.createAuthToken({
+                name: userInfo.name,
+                email: userInfo.email
+            })
+
+        } catch (error) {
+            logger.error(error);
+            // TODO: should throw an error here too?
+        }
+    });
+
+    afterAll(async () => {
+        try {
+            await database?.db.dropDatabase();
+            await disconnectDB();
+        } catch (error) {
+            logger.error(error);
+        }
+    });
+
+    it("CONTROLLER (create): Should create a new group", async () => {
+        const infoSend = {
+            name: "Test Group",
+            description: "Group for testing purposes",
+            users: [
+                {
+                    userRef: "TEST",
+                    role: "Manager"
+                }
+            ],
+            chatRoom: "test-chatroom",
+            posts: []
+        }
+
+        const route = "/group/create"
+        if (app) {
+            const res = await postRequest({ app, infoSend, route });
+            expectStatus(res, 201);
+        }
+
+    })
+
+
+
+});
