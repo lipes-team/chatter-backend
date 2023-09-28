8bee2b545b0a8baf3ddcc4c1d79d9e8233d5bcda
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-09-29 00:07:56 +0200

    update(comment controller): create a comment route

--- src/controllers/Comment.controller.ts
+++ src/controllers/Comment.controller.ts
@@ -1,5 +1,25 @@
+import { NewComment } from '../models/Comment.model';
+import { commentService } from '../services/Comment.service';
+import { RouteOpts } from '../utils/types';
+
 class CommentController {
-	async create() {}
+	async create(
+		req: RouteOpts['payload'],
+		res: RouteOpts['res'],
+		next: RouteOpts['next']
+	) {
+		try {
+			const { text } = req.body;
+			const owner = req.payload?.id!;
+			const postId = req.params.id;
+			const commentToCreate: NewComment = { text, owner, post: postId };
+			const newComment = await commentService.createComment(commentToCreate);
+			res.status(201).json(newComment);
+		} catch (error: any) {
+			error.path = 'Create comment';
+			next(error);
+		}
+	}
 	async findOne() {}
 	async update() {}
 	async delete() {}
