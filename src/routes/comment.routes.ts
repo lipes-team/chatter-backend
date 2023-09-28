d505555a1b98f5caf2fadfb846f138bf4cb8cfad
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-09-29 00:09:13 +0200

    update(comment routes): added middleware validation

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import { commentController } from '../controllers/Comment.controller';
+import { validateSchema } from '../middlewares/validateSchema';
+import { createCommentSchema } from '../validation/comment.schema';
+import { testIdSchema } from '../validation/id.schema';
 
 const router = Router();
 
-router.post('/', commentController.create);
+// This :id is actually the postId
+router.post(
+	'/:id',
+	validateSchema(testIdSchema('Create Comment')),
+	validateSchema(createCommentSchema),
+	commentController.create
+);
 router.get('/:id', commentController.findOne);
 router.put('/:id', commentController.update);
 router.delete('/:id', commentController.delete);
