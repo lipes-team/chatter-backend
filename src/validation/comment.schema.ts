ff4ef26df489bf661021b69d098a6f9aa73e31c6
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-09-29 02:58:57 +0200

    update(comments validation): added the validation for the update

--- src/validation/comment.schema.ts
+++ src/validation/comment.schema.ts
@@ -6,3 +6,10 @@ export const createCommentSchema = z.object({
 		image: z.string().optional(),
 	}),
 });
+
+export const updateCommentSchema = z.object({
+	body: z.object({
+		text: z.string().nonempty().optional(),
+		image: z.string().optional(),
+	}),
+});
